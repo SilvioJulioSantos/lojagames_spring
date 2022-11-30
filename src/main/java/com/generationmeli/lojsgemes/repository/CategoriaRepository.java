9862f0102f2488879912114d53d660e70f308513
“Silvio <silvio.dossantos@mercadolivre.com>
2022-11-29 22:11:26 -0300

    Finalizando mudanças nos métodos do projeto loja games

--- src/main/java/com/generationmeli/lojsgemes/repository/CategoriaRepository.java
+++ src/main/java/com/generationmeli/lojsgemes/repository/CategoriaRepository.java
@@ -2,9 +2,10 @@ package com.generationmeli.lojsgemes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface CategoriaRepository<Categoria> extends JpaRepository<Categoria, Long> {
 
     public List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
--- src/main/java/com/generationmeli/lojsgemes/repository/ProdutoRepository.java
+++ src/main/java/com/generationmeli/lojsgemes/repository/ProdutoRepository.java
@@ -2,10 +2,11 @@ package com.generationmeli.lojsgemes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 import java.util.List;
-
+@Repository
 public interface ProdutoRepository<Produto> extends JpaRepository<Produto, Long> {
 
     public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
